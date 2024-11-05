fc48ec2420ecefac7244398e978fe3df559f5211
Rizzy-2005 <230681@tkmce.ac.in>
2024-11-05 21:20:57 +0530

    Added movement animation.

---
+++ assets/Level/Castle_vertical1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Castle_vertical1" tilewidth="33" tileheight="32" tilecount="2" columns="1">
+ <image source="../Castle Walls/Castle_vertical1.png" width="64" height="64"/>
+</tileset>
---
+++ assets/Level/Castle_wall1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Castle_wall1" tilewidth="30" tileheight="32" tilecount="4" columns="2">
+ <image source="../Castle Walls/Castle_wall1.png" width="64" height="64"/>
+</tileset>
